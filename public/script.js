a941f4ad3ee6c898d62164783cc26b6ef5ff5dfe
ilikeyellowGZ <thisforthetimewehad@gmail.com>
2024-08-02 10:25:57 +0200

    Final commit untill Henry response, everything is done including copy, all the vwports work. nice

--- public/script.js
+++ public/script.js
@@ -66,3 +66,105 @@ document.addEventListener("click", function(event) {
         closeContactForm();
     }
 });
+
+const moreTikTokInfoBtn = document.querySelector("#tikTok .hiddenText");
+const moreAfroBeatInfoBtn = document.querySelector("#afroBeat .hiddenText");
+const moreBreakDanceInfoBtn = document.querySelector("#breakDance .hiddenText");
+const moreHipHopInfoBtn = document.querySelector("#hipHop .hiddenText");
+const moreurbanDanceInfoBtn = document.querySelector("#urbanDance .hiddenText");
+const moreAmapianoInfoBtn = document.querySelector("#amapiano .hiddenText");
+
+
+const TikTokImg = document.querySelector("#tikTok .tiktok");
+const AfroBeatImg = document.querySelector("#afroBeat .AfroBeat");
+const BreakDanceImg = document.querySelector("#breakDance .breakDance");
+const HipHopImg = document.querySelector("#hipHop .hipHop");
+const urbanDanceImg = document.querySelector("#urbanDance .UrbanDance");
+const AmapianoImg = document.querySelector("#amapiano .Amapiano");
+
+const TikTokReadLess = document.querySelector("#tikTok .ReadLess");
+const AfroBeatReadLess = document.querySelector("#afroBeat .ReadLess");
+const BreakDanceReadLess = document.querySelector("#breakDance .ReadLess");
+const HipHopReadLess = document.querySelector("#hipHop .ReadLess");
+const urbanDanceReadLess = document.querySelector("#urbanDance .ReadLess");
+const AmapianoReadLess = document.querySelector("#amapiano .ReadLess");
+
+const TikTokReadMore = document.querySelector("#tikTok .ReadMore");
+const AfroBeatReadMore = document.querySelector("#afroBeat .ReadMore");
+const BreakDanceReadMore = document.querySelector("#breakDance .ReadMore");
+const HipHopReadMore = document.querySelector("#hipHop .ReadMore");
+const urbanDanceReadMore = document.querySelector("#urbanDance .ReadMore");
+const AmapianoReadMore = document.querySelector("#amapiano .ReadMore");
+
+TikTokReadMore.addEventListener("click", () => {
+    TikTokReadMore.classList.add("none");
+    TikTokReadLess.classList.remove("none");
+    moreTikTokInfoBtn.classList.remove("none");
+});
+
+TikTokReadLess.addEventListener("click", () => {
+    TikTokReadMore.classList.remove("none");
+    TikTokReadLess.classList.add("none");
+    moreTikTokInfoBtn.classList.add("none");
+});
+
+AfroBeatReadMore.addEventListener("click", () => {
+    AfroBeatReadMore.classList.add("none");
+    AfroBeatReadLess.classList.remove("none");
+    moreAfroBeatInfoBtn.classList.remove("none");
+});
+
+AfroBeatReadLess.addEventListener("click", () => {
+    AfroBeatReadMore.classList.remove("none");
+    AfroBeatReadLess.classList.add("none");
+    moreAfroBeatInfoBtn.classList.add("none");
+});
+
+BreakDanceReadMore.addEventListener("click", () => {
+    BreakDanceReadMore.classList.add("none");
+    BreakDanceReadLess.classList.remove("none");
+    moreBreakDanceInfoBtn.classList.remove("none");
+});
+
+BreakDanceReadLess.addEventListener("click", () => {
+    BreakDanceReadMore.classList.remove("none");
+    BreakDanceReadLess.classList.add("none");
+    moreBreakDanceInfoBtn.classList.add("none");
+});
+
+HipHopReadMore.addEventListener("click", () => {
+    HipHopReadMore.classList.add("none");
+    HipHopReadLess.classList.remove("none");
+    moreHipHopInfoBtn.classList.remove("none");
+});
+
+HipHopReadLess.addEventListener("click", () => {
+    HipHopReadMore.classList.remove("none");
+    HipHopReadLess.classList.add("none");
+    moreHipHopInfoBtn.classList.add("none");
+});
+
+urbanDanceReadMore.addEventListener("click", () => {
+    urbanDanceReadMore.classList.add("none");
+    urbanDanceReadLess.classList.remove("none");
+    moreurbanDanceInfoBtn.classList.remove("none");
+});
+
+urbanDanceReadLess.addEventListener("click", () => {
+    urbanDanceReadMore.classList.remove("none");
+    urbanDanceReadLess.classList.add("none");
+    moreurbanDanceInfoBtn.classList.add("none");
+});
+
+AmapianoReadMore.addEventListener("click", () => {
+    AmapianoReadMore.classList.add("none");
+    AmapianoReadLess.classList.remove("none");
+    moreAmapianoInfoBtn.classList.remove("none");
+});
+
+AmapianoReadLess.addEventListener("click", () => {
+    AmapianoReadMore.classList.remove("none");
+    AmapianoReadLess.classList.add("none");
+    moreAmapianoInfoBtn.classList.add("none");
+});
+
